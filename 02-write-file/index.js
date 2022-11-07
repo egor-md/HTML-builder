15f148d66e78869c924cb9656e61159bdc6e6930
egor-md <107609106+egor-md@users.noreply.github.com>
2022-11-08 01:25:50 +0300

    Update index.js

---
+++ 02-write-file/index.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+
+let pathToFile = __dirname + '\\text.txt';
+let output = fs.createWriteStream(pathToFile, 'utf-8');
+
+const process = require('process');
+process.stdout.write('Hi, enter text please');
+
+process.on('SIGINT', function () {
+    process.exit();
+});
+
+process.stdin.on('data', function (print) {
+    if (print.toString().trim() === 'exit') {
+        process.exit();
+    } else {
+        output.write(print);
+    }   
+});
+
+process.on('exit', function () {
+    process.stdout.write('file with your test was created');
+});
