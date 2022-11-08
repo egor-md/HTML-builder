c12bfcc3ce6f76ee193830a8fc5151557b57cd0a
egor-md <107609106+egor-md@users.noreply.github.com>
2022-11-08 20:16:48 +0300

    fix: path_way

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,19 +1,24 @@
+const { log } = require('console');
 const { stat } = require('fs');
 const { readdir } = require('fs/promises');
 const path = require('path');
 
-let pathToFiles = __dirname + '\\secret-folder';
+let pathToFiles = path.join(__dirname, 'secret-folder');
 
-readdir(pathToFiles, { withFileTypes: true }).then(res => res.forEach(i => {
+readdir(pathToFiles, { withFileTypes: true })
+    .then(res => res.forEach(i => {
 
-    let pathToF = path.join(pathToFiles, i.name);
+        let pathToF = path.join(pathToFiles, i.name);
 
-    if (i.isFile()) {
-        stat(pathToF, (err, stats) => {
-            let fName = i.name.split('.')[0];
-            let fExt = path.extname(pathToF).slice(1);
-            let fSize = (stats.size / 1024).toFixed(3);
-            return console.log(`${fName} - ${fExt} - ${fSize}kb`);
-        });
-    }
-}));
+        if (i.isFile()) {
+            stat(pathToF, (err, stats) => {
+                let fName = i.name.split('.')[0];
+                let fExt = path.extname(pathToF).slice(1);
+                let fSize = (stats.size / 1024).toFixed(3);
+                return console.log(`${fName} - ${fExt} - ${fSize}kb`);
+            });
+        }
+    }))
+    .catch(err => {
+        console.log(err);
+    });
